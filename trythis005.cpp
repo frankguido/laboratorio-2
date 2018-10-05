56bc2926c861985a5d98417905b38afbb1f4334f
frankguido <38305115+frankguido@users.noreply.github.com>
2018-10-04 22:43:45 -0500

    Create trythis005.cpp

---
+++ trythis005.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include<vector>
+#include<string>
+
+
+using namespace std;
+int main()
+{
+	vector<string> palabras;
+	palabras.push_back("carajo);
+	palabras.push_back("mierda");
+	palabras.push_back("puta");
+	string temp;
+	cout<<"numero de palabras "<<words.size()<<endl;
+	while(cin>>temp)
+	{
+		
+	
+	for (int i=0;i<words.size();i++)
+		{
+			if(words[i-1]!=words[i])
+				cout<<words[i]<<"\n";
+		}
+}
