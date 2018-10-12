b8c1c071c0bd5b5fb38c2b34a7b382d2ff9e16b0
frankguido <38305115+frankguido@users.noreply.github.com>
2018-10-12 16:23:50 -0500

    Update quest006.cpp

--- quest006.cpp
+++ quest006.cpp
@@ -27,3 +27,33 @@ int main ()
 	cout<<a,b,c;
  return 0;	
 }
+
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main ()
+{
+	unsigned long long x;
+
+	for( int i=1;i<=64;i++)
+	{
+		x= pow(2,i-1);
+		cout<<i<<" \t"<< x <<"\n";
+		if(x<=1000)
+		{
+			cout<<i;
+		}
+		if(x<=10000000)
+		{
+			cout<<i;
+		}
+	
+		if(x<=10000000)
+		{
+			cout<<i;
+		}
+	}
+
+
+ return 0;	
+}
