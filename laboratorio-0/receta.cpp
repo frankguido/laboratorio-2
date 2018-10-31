ba43e2f798a1a59cdf5f83fdb00edbdac09011ff
frankguido <38305115+frankguido@users.noreply.github.com>
2018-10-31 09:05:29 -0500

    Create receta.cpp

---
+++ laboratorio-0/receta.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"           SUSPIRO A LA LIMENA "<<endl;
+	cout<<">>INGREDIENTES:"<<endl;
+	cout<<"2 latas de leche condensada."<<endl;
+	cout<<"2 latas de leche evaporada."<<endl;
+	cout<<"8 yemas."<<endl;
+	cout<<"1 taza de oporto."<<endl;
+	cout<<"1 1/2 de azucar."<<endl;
+	cout<<"6 claras."<<endl;
+	cout<<"2 cucharadas de canela en polvo."<<endl;
+	cout<<">>PREPARACION:"<<endl;
+	cout<<"Vierta la leche condensada y la leche evaporada en una olla de fondo grueso"<<endl;
+	cout<<"Lleve a fuego bajo sin dejar de mover con una cuchara de madera."<<endl;
+	cout<<"Cocine durante una hora, aproximadamente, hasta que pueda ver el fondo de la olla."<<endl;
+	cout<<"Cuide de mover adecuadamente para que no se queme. "<<endl;
+	cout<<"Retire del fuego y agregue inmediatamente las yemas batidas, en forma de hilo. "<<endl;
+	cout<<"Remueva energicamente. "<<endl;
+	cout<<"Deje enfriar y vierta el manjar en dulceras."<<endl;
+	cout<<"Para el merengue, mezcle en una olla pequena el oporto con el azucar y lleve a fuego bajo hasta que se disuelva. "<<endl;
+	cout<<"Suba a fuego medio y deje hervir, sin mover, hasta formar un almibar espeso."<<endl;
+	cout<<"Bata las claras con batidora, cuando se formen picos, agregue el almibar caliente en forma de hilo continuo."<<endl;
+	cout<<"Debe obtener un merengue firme. "<<endl;
+	cout<<"Llene una manga con merengue y decore las dulceras."<<endl;
+	cout<<"Esparza canela molida."<<endl;
+	return 0;
+	
+}
