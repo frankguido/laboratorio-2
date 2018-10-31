355bdfea457c9f88ecfedf92f15e5050ba143bc8
frankguido <38305115+frankguido@users.noreply.github.com>
2018-10-31 09:38:26 -0500

    Add files via upload

---
+++ laboratorio-1/calculadoraconfloat.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float a,b;
+	cout<<"ingrese valores";
+	cin>>a>>b;
+	cout<<"la suma es :"<<a+b<<endl;
+	cout<<"la resta es :"<<a-b<<endl;
+	cout<<"la multiplicacion es :"<<a*b<<endl;
+	cout<<"la divicion es :"<<a/b<<endl;
+	if (a>b)
+	{
+		cout<<a<<" es mayor que "<<b<<endl;
+	}
+	else
+		cout<<b<<" es mayor que "<<a<<endl;
+		
+	
+	
+}
---
+++ laboratorio-1/calculadoranormal.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b;
+	cout<<"ingrese valores";
+	cin>>a>>b;
+	cout<<"la suma es :"<<a+b<<endl;
+	cout<<"la resta es :"<<a-b<<endl;
+	cout<<"la multiplicacion es :"<<a*b<<endl;
+	cout<<"la divicion es :"<<a/b<<endl;
+	if (a>b)
+	{
+		cout<<a<<"es mayor que"<<b<<endl;
+	}
+	else
+		cout<<a<<"es mayor que"<<b<<endl;
+	
+	
+}
