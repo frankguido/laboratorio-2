9a524977d1d45c303c3d9a53dc729908277c45ea
frankguido <38305115+frankguido@users.noreply.github.com>
2018-11-02 15:46:00 -0500

    Add files via upload

---
+++ laboratorio-3/cod01.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+int area ( int length , int width );
+int main ( )
+{
+//int s1 = area (7; // e r r o r : f a l t a )
+//int s1=area (7) // e r r o r : f a l t a ;
+//Int s3=area (7); // e r r o r : I n t no e s un t i p o
+int s4=area (7); // e r r o r : c a r a c t e r ( f a l t a ? )
+return area (4,4);
+}
+int area( int length , int width )
+{
+return length * width ;
+}
+
+
