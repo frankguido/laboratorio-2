89f7ecc1cad5282406e6d2e604d9edcd56674988
frankguido <38305115+frankguido@users.noreply.github.com>
2018-11-02 15:46:35 -0500

    Create cod02.cpp

---
+++ laboratorio-3/cod02.cpp
@@ -0,0 +1,16 @@
+#include<iostreram>
+
+int area ( int length , int width );
+int main ( )
+{
+
+int x0=arena (7); // e r r o r : f a l t a ;
+Int x1=area (7); // e r r o r : I n t no e s un t i p o
+int x2=area ("seven",2); // e r r o r : c a r a c t e r ( f a l t a � )
+return area (4,4);
+}
+int area( int length , int width )
+{
+return length * width ;
+}
+
