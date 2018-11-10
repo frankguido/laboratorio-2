c0cfe7f6b342389130fc8068245f11b4d83b91d1
frankguido <38305115+frankguido@users.noreply.github.com>
2018-11-10 12:47:59 -0500

    Create cod04.cpp

---
+++ laboratorio-4/cod04.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+int f(int);
+int main()
+{
+	int i=7;
+	cout<<f(i)<<'\n';	
+}
+double my_sqrt(double);
+extern int a;
+int a;
+int x=7;
+extern int x;
+extern int x;
+double my_sqrt(double);
+double my_sqrt(double d)
+{
+	return 0.0;
+}
+double my_sqrt(double);
+double my_sqrt(double);
+double expression();
+double primary()
+{
+	return expression();
+}
+double term()
+{
+	return primary();
+}
+double expression()
+{
+	return term();
+}
+int f(int n)
+{
+	return n;
+}
