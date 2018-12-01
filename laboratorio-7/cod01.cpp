0182a5021568c08b81e70178d14d959c551bbc22
frankguido <38305115+frankguido@users.noreply.github.com>
2018-12-01 10:58:55 -0500

    Add files via upload

---
+++ laboratorio-7/cod01.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+	cout<<"el tam de char es"<<sizeof(char)<<" "<<sizeof('a')<<'\n';
+	cout<<"el tam de int es"<<sizeof(int)<<" "<<sizeof(2+2)<<'\n';
+	int *p=0;
+	cout<<"el tam de int es"<<sizeof(int*)<<" "<<sizeof(p)<<'\n';
+	vector<int>v(1000);
+	cout<<"el tam de vecor<int>(1000) es"<<sizeof(vector<int>)<<" "<<sizeof(v)<<'\n';
+}
